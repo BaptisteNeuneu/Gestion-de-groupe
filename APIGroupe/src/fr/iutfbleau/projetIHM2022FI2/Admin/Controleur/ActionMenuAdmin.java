9f2626127c85a9ae0e08f5f2c679057d2e40fbb8
BAPTISTE NEVEJANS <baptisrenevejans7@gmai.com>
2022-12-07 14:36:35 +0100

    mise a jour panel

--- APIGroupe/src/fr/iutfbleau/projetIHM2022FI2/Admin/Controleur/ActionMenuAdmin.java
+++ APIGroupe/src/fr/iutfbleau/projetIHM2022FI2/Admin/Controleur/ActionMenuAdmin.java
@@ -2,6 +2,8 @@ package fr.iutfbleau.projetIHM2022FI2.Admin.Controleur;
 
 import java.awt.event.*;
 import javax.swing.*;
+import fr.iutfbleau.projetIHM2022FI2.MNP.*;
+import fr.iutfbleau.projetIHM2022FI2.API.*;
 
 import fr.iutfbleau.projetIHM2022FI2.Admin.Vue.*;
 
@@ -9,7 +11,7 @@ public class ActionMenuAdmin implements ActionListener {
 
     JButton AddEtu,CreeGroup,SupGroup,RenGroup,DepEtu;
     Pancardlayout a;
-    public ActionMenuAdmin(JButton AddEtu,JButton CreeGroup,JButton SupGroup,JButton RenGroup,JButton DepEtu,Pancardlayout a){
+    public ActionMenuAdmin(JButton AddEtu,JButton CreeGroup,JButton SupGroup,JButton RenGroup,JButton DepEtu,Pancardlayout a,AbstractGroupeFactory agf){
         this.AddEtu = AddEtu;
         this.CreeGroup = CreeGroup;
         this.SupGroup = SupGroup;
--- APIGroupe/src/fr/iutfbleau/projetIHM2022FI2/Admin/MainAdmin.java
+++ APIGroupe/src/fr/iutfbleau/projetIHM2022FI2/Admin/MainAdmin.java
@@ -1,10 +1,10 @@
 package fr.iutfbleau.projetIHM2022FI2.Admin;
 
 import fr.iutfbleau.projetIHM2022FI2.Admin.Vue.*;
+import fr.iutfbleau.projetIHM2022FI2.Modele.*;
 
 public class MainAdmin {
     public static void main(String[] args) {
-        VueAdmin v = new VueAdmin();
-        v.Vue1();
+        new Initialisation();
     }
 }
